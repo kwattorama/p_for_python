7443c1e88a10e4fba5eb2da12863b47a07360a48
kwattorama <61426179+kwattorama@users.noreply.github.com>
2020-09-21 22:39:51 +0530

    Create simple_cal-c.py

---
+++ simple_cal-c.py
@@ -0,0 +1,57 @@
+import time
+import math
+
+
+print("\n to the  calc\n")
+
+while True:
+    def _input():
+        global x
+        global y
+        x = float(input("Enter the 1st number: "))
+        y = float(input("Enter the 2nd number: "))
+        return x, y
+
+    time.sleep(4.0)
+    operations = ["Addition", "Subtraction", "Multiplication", "Divion",
+                "Square", "Square root", "Factorial", "Angle conversion", "Exit"]
+
+    for idx, operation in enumerate(operations, start=1):
+        print(f"{idx}. {operation}")
+
+    calc = int(input("Choose the operation >>> "))
+
+    if calc == 1:
+        _input()
+        print(f"{x} + {y}  = {x + y}")
+    elif calc == 2:
+        _input()
+        print(f"{x} - {y}  = {x - y}")
+    elif calc == 3:
+        _input()
+        print(f"{x} * {y}  = {x * y}")
+    elif calc == 4:
+        _input()
+        print(f"{x} / {y}  = {x / y}")
+    elif calc == 5:
+        x = int(input("Enter the number: "))
+        print(f"{x}^2 = {x ** 2}")
+    elif calc == 6:
+        x = int(input("Enter the number: "))
+        print(f"square root of {x} = {x ** 0.5}")
+    elif calc == 7:
+         x = int(input("Enter the number: "))
+         print(f"{x}! = {math.factorial(x)}")
+    elif calc == 8:
+        angle = input("\n1. rad to degree\n2.degree to rad\nPress[1/2]>>> ")
+
+        if angle == "1":
+            x = int(input("Radian: "))
+            
+            print
+        else:
+            pass        
+    elif calc == 9:
+        break
+    else:
+        print("Invalid choice")

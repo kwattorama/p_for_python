cb85d7ea4a8348cc00035649bbe18571726ef5cd
kwattorama <61426179+kwattorama@users.noreply.github.com>
2020-09-28 22:53:38 +0530

    Update trade_decision_maker.py

--- trade_decision_maker.py
+++ trade_decision_maker.py
@@ -5,7 +5,8 @@
 # ======================================================================
 
 # TODO(kwattorama)
-# - add RSI, FIBO, support and resistane input
+# - add FIBO, support and resistane input
+# - make decision based on user input
 # - calculate reward, risk and RR ratio
 # -use dict for the whole details
 
@@ -26,7 +27,7 @@ indicators = {
                            "Hanging man", "Spinning top", "Doji"]
     },
     "trend": ["Uptrend", "Downtrend", "Sideway"],
-    "RSI": ["Overbrought", "Oversold", "Average"],
+    "rsi": ["Overbrought", "Oversold", "Average"],
     "chart_pattern": ["Double Top", "Head & Shoulders", "Inverted H & S",
                       "Cup & Handle", "Double bottom", "No comfirm pattern"],
     "breakdown": ["Triangle breakdown", "Rectangular breakdown",
@@ -40,35 +41,56 @@ print("\n")
 for tf_idx, time_frames in enumerate(indicators["time_frames"], start=1):
   print(f"{tf_idx}. {time_frames}")
 
-your_tf = int(input("Choose your time frame>>> "))
+your_tf = int(input("Choose your time frame >>> "))
 print("\n")
 # ltp = last traded price
-ltp = int(input("Enter the close of the last candle>>> "))
-print("\n")
+ltp = int(input("Enter the close of the last candle >>> "))
+print(f"{'*' * 50}")
 
 for ema_idx, ema in enumerate(indicators["ema"], start=1):
   print(f"{ema_idx}. {ema}")
 
-ema = input("Choose the ema crossing type if any>>> ")
-print("\n")
+ema = input("Choose the ema crossing type if any >>> ")
+print(f"{'*' * 50}")
 
 for volume_idx, volume in enumerate(indicators["volume"], start=1):
   print(f"{volume_idx}. {volume}")
 
-volume = input("Volume is... >>>")
-print("\n")
+volume = input("Volume is... >>> ")
+print(f"{'*' * 50}")
 
-for chart_idx, chart_pattern in enumerate(indicators["candle_pattern"].values(), start=1):
-  print(f"Group-{chart_idx}.{chart_pattern}\n")
+for candle_idx, candle_pattern in enumerate(indicators["candle_pattern"].values(), start=1):
+  print(f"{candle_idx}.{candle_pattern}\n")
  
 candle_pattern = input(
-    f"\nIn \"{script_name}\" chart is there any candlestick pattern from the any one of the above group?>>> ")
-    print("\n")
+    f"\nIn \"{script_name}\" chart is there any candlestick pattern from any one of the above group? >>> ")
+print(f"{'*' * 50}")
 
 for trend_idx, trend in enumerate(indicators["trend"], start=1):
-  print(f"Group-{trend_idx}.{trend}\n")
+  print(f"{trend_idx}.{trend}")
 
-your_tf_trend = input(f"Which trend is in your time frame[1/2/3]>>> ")
-guide_tf_trend = input("Which trend is in guide time frame[1/2/3]>>> ")
-lower_tf_trend = input("Which trend is in lower time frame[1/2/3]>>> ")
-print("\n")
+your_tf_trend = input(f"Which trend is in your time frame[1/2/3] >>> ")
+guide_tf_trend = input("Which trend is in guide time frame[1/2/3] >>> ")
+lower_tf_trend = input("Which trend is in lower time frame[1/2/3] >>> ")
+print(f"{'*' * 50}")
+
+
+for rsi_idx, rsi in enumerate(indicators["rsi"], start=1):
+  print(f"{rsi_idx}.{rsi}")
+  
+trend = input(f"RSI is..? >>> ")
+
+for chart_idx, chart_pattern in enumerate(indicators["chart_pattern"], start=1):
+    print(f"{chart_idx}.{chart_pattern}")
+  
+trend = input(f"RSI is..? >>> ")
+
+for breakdown_idx, breakdown in enumerate(indicators["breakdown"], start=1):
+  print(f"{breakdown_idx}.{breakdown}")
+  
+trend = input(f"Choose any breakdown occuring/may occur in guide time frame and your time frame >>> ")
+
+for divergence_idx, divergence in enumerate(indicators["divergence"], start=1):
+  print(f"{divergence_idx}.{divergence}")
+  
+trend = input(f"Any divergence? >>> ")
